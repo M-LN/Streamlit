1dd636e93c4d4d09175937478699a2b2c34eb83b
M-LN <m.lundum.andersen@gmail.com>
2025-02-10 13:19:02 +0100

    new file:   Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# File Uploader
+st.title('Anxiety and Depression Analysis App')
+uploaded_file = st.file_uploader('Upload your CSV file', type='csv')
+
+if uploaded_file is not None:
+    # Load the new dataset
+    df = pd.read_csv(uploaded_file)
+    st.write("Here are the columns in your dataset:")
+    st.write(df.columns)  # Display column names
+
+    # Display first few rows of the dataset
+    st.write("Here are the first few rows of your dataset:")
+    st.write(df.head())
+
+    # Display basic statistics of the dataset
+    st.write("Basic statistics of your dataset:")
+    st.write(df.describe())
+
+    # Sidebar input for column selection
+    selected_column = st.sidebar.selectbox('Select a column to search', df.columns)
+
+    # Sidebar input for search term
+    search_term = st.sidebar.text_input('Enter search term')
+    #Display the search results
+    search_results = df[df[selected_column].str.contains(search_term, case=False)]
+    st.write(search_results)
+
+    # Display a bar plot of the selected column
+    st.write('Bar plot of the selected column:')
+    sns.countplot(x=selected_column, data=df)
+    plt.xticks(rotation=45)
+    st.pyplot()
+else:
+    st.write('Please upload a CSV file to proceed.')
